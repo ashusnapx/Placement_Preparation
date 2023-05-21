faeca7de7ee20274d1ad48aa0317396e9fb68d1a
Ashutosh Kumar <75922918+ashusnapx@users.noreply.github.com>
2023-05-21 16:38:24 +0530

    initial checking...

---
+++ 01_Sorting/00_selection_sort.cpp
@@ -0,0 +1,12 @@
+/* so basically in selection sort, the algorithm chooses the minimum or lowest
+element from list, then replaces it with the first element. and this goes on.
+
+Step 1 - Find out the minimum from array or vector or list
+Step 2 - Swap it with element at first place
+Step 3 - Now elements after 0th index are unsorted
+Step 4 - Find out the minimum from 1st index to last index
+Step 5 - Swap it with element at 1st index
+Repeat
+
+TODO: Get the Minimum and Swap it.
+*/
