644ab0d3b199b65c0378d2b1853bc49230453240
Ashutosh Kumar <75922918+ashusnapx@users.noreply.github.com>
2023-05-21 19:12:40 +0530

    code check

--- 01_Sorting/01_bubble_sort.cpp
+++ 01_Sorting/01_bubble_sort.cpp
@@ -1,14 +1,14 @@
 /* so basically in selection sort, the algorithm chooses the minimum or lowest
 element from list, then replaces it with the first element. and this goes on.
 
-Step 1 - Find out the minimum from array or vector or list
-Step 2 - Swap it with element at first place
-Step 3 - Now elements after 0th index are unsorted
-Step 4 - Find out the minimum from 1st index to last index
+Step 1 - Compare the first two elements
+Step 2 - Swap it if arr[i] > arr[i+1]
+Step 3 - if not, then move forward
+Step 4 - In first round the maximum element will come  at last position
 Step 5 - Swap it with element at 1st index
 Repeat
 
-TODO: Get the Minimum and Swap it.
+TODO: Pushes the maximum to the last by adjacent swaps.
 */
 
 #include <bits/stdc++.h>
@@ -54,7 +54,3 @@ int main() {
 
   return 0;
 }
-
-/* 
-Time complexity : O(n) <-- best, average & worst
- */
