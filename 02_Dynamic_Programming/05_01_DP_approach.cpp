4dc0e1a24d5cd13dd1ec7f85ac390f102ac7dd78
Ashutosh Kumar <75922918+ashusnapx@users.noreply.github.com>
2023-06-02 16:08:23 +0530

    Day - 4

---
+++ 02_Dynamic_Programming/05_01_DP_approach.cpp
@@ -0,0 +1,47 @@
+/* Question basically yeh bol rha hai ki agar tumhare pas ek list hai.....maan lo [1,3,4,5] toh jaise 1 ke adjacent 3 hai, 3 ke 4, 4 ke 5, toh unko add nhi kr sakte
+jaise ki maan lo chosen number hai:
+1: 4 + 5 = 9
+3: 5 = 5
+4: 1 = 1
+5: 1 + 3 = 4
+
+so if asked maximum then 1 is the answer and if min then 4.
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int helper(int index, vector<int> &nums, vector<int> &dp)
+{
+    // if their is only a single element in the vector, so it can't be compared with others. so when function maximum_non_adjacent_sum passes size - 1 = 1 - 1 = 0
+    if(index == 0) return nums[index];
+
+    // this is an edge case as question mentioned that we cannot consider the subsequent element and to satisfy that condition we need to check index - 2 every time and if at any case then 0th index crosses so we put a check so that no error may come.
+    if(index < 0) return 0;
+
+    if(dp[index] != -1) return dp[index];
+
+    int pick = nums[index] + helper(index - 2, nums, dp);
+
+    int not_pick = 0 + helper(index - 1, nums, dp);
+
+    return dp[index] = max(pick, not_pick);
+}
+
+int maximum_non_adjacent_sum(vector<int> nums)
+{
+    int n = nums.size();
+    vector<int> dp(n, -1);
+    return helper(n-1, nums, dp);
+}
+
+int main()
+{
+    vector<int> nums;
+    nums.push_back(2);
+    nums.push_back(1);
+    nums.push_back(4);
+    nums.push_back(9);
+
+    cout << maximum_non_adjacent_sum(nums);
+}
---
+++ 02_Dynamic_Programming/05_02_Tabulation.cpp
@@ -0,0 +1,46 @@
+/* Question basically yeh bol rha hai ki agar tumhare pas ek list hai.....maan lo [1,3,4,5] toh jaise 1 ke adjacent 3 hai, 3 ke 4, 4 ke 5, toh unko add nhi kr sakte
+jaise ki maan lo chosen number hai:
+1: 4 + 5 = 9
+3: 5 = 5
+4: 1 = 1
+5: 1 + 3 = 4
+
+so if asked maximum then 1 is the answer and if min then 4.
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int maximum_non_adjacent_sum(vector<int> nums)
+{
+    int n = nums.size();
+    int prev = nums[0];
+    int prev2 = 0;
+    for(int i = 1 ; i < n ; i++)
+    {
+        int take = nums[i];
+        if(i > 1)
+        {
+            take += prev2;
+        }
+
+        int not_take = 0 + prev;
+
+        int current_i = max(take, not_take);
+        prev2 = prev;
+        prev = current_i;
+    }
+
+    return prev;
+}
+
+int main()
+{
+    vector<int> nums;
+    nums.push_back(2);
+    nums.push_back(1);
+    nums.push_back(4);
+    nums.push_back(9);
+
+    cout << maximum_non_adjacent_sum(nums);
+}
