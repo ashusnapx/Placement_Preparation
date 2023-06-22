592905536e015dd411fd27271d392805b2d8e2a6
Ashutosh Kumar <75922918+ashusnapx@users.noreply.github.com>
2023-06-22 10:55:50 +0530

    day 5

---
+++ 02_Dynamic_Programming/18_01_count_partitions_with_difference_memoization.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h> 
+int mod = 1e9 + 7;
+int helper(int index, int target, vector<int> &arr, vector<vector<int>> &dp)
+{
+    /* base case */
+    if(index == 0)
+    {
+        if(target == 0 and arr[0] == 0) return 2;
+        if(target == 0 or arr[0] == target) return 1;
+        return 0;
+    }
+
+    if(dp[index][target] != -1)
+    {
+        return dp[index][target];
+    }
+
+    /* pick and not pick */
+    int notTake = 0 + helper(index - 1, target, arr, dp);
+    int take = 0;
+    if(target >= arr[index])
+    {
+        take = helper(index - 1, target - arr[index], arr, dp);
+    }
+    return dp[index][target] = (take + notTake) % mod;
+}
+int countPartitions(int n, int d, vector<int> &arr) {
+    int sum = 0;
+    for(int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+    if (sum - d < 0 || (sum - d) % 2 != 0) {
+        return 0; // No valid partition exists
+    }
+    int updatedSum = (sum - d) / 2;
+    vector<vector<int>> dp(n, vector<int>(updatedSum+1, -1));
+    return helper(n-1, updatedSum, arr, dp);
+}
