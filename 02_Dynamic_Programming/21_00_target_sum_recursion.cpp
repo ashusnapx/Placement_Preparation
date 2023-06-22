8c47b9180c543f6b82b80e3044dc23f368d77a30
Ashutosh Kumar <75922918+ashusnapx@users.noreply.github.com>
2023-06-22 12:02:44 +0530

    done by just seeing explanation

--- 02_Dynamic_Programming/21_00_target_sum_recursion.cpp
+++ 02_Dynamic_Programming/21_00_target_sum_recursion.cpp
@@ -1,12 +1,32 @@
 #include <bits/stdc++.h> 
+int mod = (int)(1e9+7);
 int helper(int index, int target, vector<int> &arr)
 {
+    // base case
     if(index == 0)
     {
-        /* check if the first element is totally divisible by target */
-        if()
+        if(target == 0 and arr[0] == 0) return 2;
+        if(target == 0 or arr[0] == target) return 1;
+        return 0;
     }
+
+    int notTake = helper(index - 1, target, arr);
+    int take = 0;
+    if(target >= arr[index])
+    {
+        take = helper(index - 1, target - arr[index], arr);
+    }
+    return (take + notTake) % mod ;
 }
-int targetSum(int n, int target, vector<int>& arr) {
-    
+int targetSum(int n, int d, vector<int>& arr) {
+    int sum = 0;
+    for(int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+    if (sum - d < 0 || (sum - d) % 2 != 0) {
+        return 0; // No valid partition exists
+    }
+    int updatedSum = (sum - d) / 2;
+    return helper(n-1, updatedSum, arr);
 }
