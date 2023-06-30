75bed7a8efe508e15efb9168e7928ab9123ff1c4
Ashutosh Kumar <75922918+ashusnapx@users.noreply.github.com>
2023-06-30 10:51:54 +0530

    zig zag done!

---
+++ 08_Trees/19_01_zig_zag_traversal_gfg.cpp
@@ -0,0 +1,39 @@
+vector <int> zigZagTraversal(Node* root)
+    {
+    	vector<int> result; // To store the traversal result
+    
+    if (root == nullptr)
+        return result; // Return an empty vector if the tree is empty
+    
+    queue<Node*> q;
+    q.push(root);
+    
+    bool leftToRight = true; // Flag to indicate the current direction of traversal
+    
+    while (!q.empty()) {
+        int size = q.size();
+        vector<int> level(size); // To store the elements of the current level
+        
+        for (int i = 0; i < size; i++) {
+            Node* node = q.front();
+            q.pop();
+            
+            int index = (leftToRight) ? i : (size - 1 - i); // Determine the index based on the traversal direction
+            
+            level[index] = node->data; // Store the node's data in the level vector
+            
+            // Add the left and right children of the current node to the queue
+            if (node->left)
+                q.push(node->left);
+            if (node->right)
+                q.push(node->right);
+        }
+        
+        leftToRight = !leftToRight; // Flip the traversal direction for the next level
+        
+        // Append the current level's elements to the result vector
+        result.insert(result.end(), level.begin(), level.end());
+    }
+    
+    return result;
+    }
