fcfc58bddae7967b56d36876badd94902fdf3062
Ashutosh Kumar <75922918+ashusnapx@users.noreply.github.com>
2023-07-01 19:50:31 +0530

    brute

---
+++ 06_Binary_Search/12_single_element_brute.cpp
@@ -0,0 +1,30 @@
+int singleNonDuplicate(vector<int>& arr)
+{
+	int n = arr.size();
+	if(n == 0) return arr[0];
+	for(int i = 0; i < n; i++)
+	{
+		if(i == 0)
+		{
+			if(arr[i+1] != arr[i])
+			{
+				return arr[i];
+			}
+		}
+		else if(i == n - 1)
+		{
+			if(arr[i] != arr[i-1])
+			{
+				return arr[i];
+			}
+		}
+		else
+		{
+			if(arr[i] != arr[i+1] and arr[i] != arr[i-1])
+			{
+				return arr[i];
+			}
+		}
+	}
+	return -1;
+}
