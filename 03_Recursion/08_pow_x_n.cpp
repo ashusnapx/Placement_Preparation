671fede935b5f761d5afaa03a6aa965d529fb54d
Ashutosh Kumar <75922918+ashusnapx@users.noreply.github.com>
2023-07-10 21:37:02 +0530

    pow(x, n) implemented

--- 03_Recursion/08_pow_x_n.cpp
+++ 03_Recursion/08_pow_x_n.cpp
@@ -1,3 +1,30 @@
 double myPow(double x, int n) {
-    // Write Your Code Here
+    double ans = 1.0;
+
+    // create a copy of n, so that even if it gets destroyed, no worries
+    long long new_n = n;
+
+    // now n can be +ve or it can be -ve, so if it is -ve then i can basically
+    // multiply it with -1 and return ans as 1/ans
+    if(new_n < 0) new_n = -1 * new_n;
+
+    // traverse the number of times n exist
+    while(new_n!=0)
+    {
+        // if n is even
+        if(new_n % 2 == 0)
+        {
+            x = x * x;
+            new_n = new_n / 2;
+        }
+        // if n is odd
+        else
+        {
+            ans = ans * x;
+            new_n--;
+        }
+    }
+
+    if(n < 0) return (double)(1.0) / (double)(ans);
+    return ans;
 }
